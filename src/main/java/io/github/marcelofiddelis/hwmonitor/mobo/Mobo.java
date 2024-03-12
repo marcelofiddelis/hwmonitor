5f001393dacb77180e7ac667e0706e1e3688fcd0
Marcelo Fidelis <marcelofiddelis201@gmail.com>
2024-03-12 11:55:17 -0300

    feat: Added mobo interface

--- src/main/java/io/github/marcelofiddelis/hwmonitor/mobo/Mobo.java
+++ src/main/java/io/github/marcelofiddelis/hwmonitor/mobo/Mobo.java
@@ -3,5 +3,28 @@ package io.github.marcelofiddelis.hwmonitor.mobo;
  * Responsible interface for mobo (Motherboard) monitor
  */
 public interface Mobo {
-    
+
+    /**
+     * 
+     * @return The Motherboard family
+     */
+    public String family();
+
+    /**
+     * 
+     * @return Motherboard socket
+     */
+    public String socket();
+
+
+    /**
+     * 
+     * @return Motherboard manufacture. 
+     * 
+     * If the information cannot be found, the default value is 'OEM'
+     * 
+     */
+    public String manufacture();
+
+
 }
