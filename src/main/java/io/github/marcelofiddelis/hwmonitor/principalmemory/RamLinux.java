66e71b87c540760509ab068f0797f24425f1344b
Marcelo Fidelis <marcelofiddelis201@gmail.com>
2024-03-23 21:33:33 -0300

    feat: Added linux ram getter implementation

--- src/main/java/io/github/marcelofiddelis/hwmonitor/principalmemory/RamLinux.java
+++ src/main/java/io/github/marcelofiddelis/hwmonitor/principalmemory/RamLinux.java
@@ -1,17 +1,30 @@
 package io.github.marcelofiddelis.hwmonitor.principalmemory;
 
+import io.github.marcelofiddelis.hwmonitor.utils.filehandler.FileHandlerLinux;
+
 public class RamLinux implements Ram {
 
+    private static RamLinux ramLinuxInstance;
+    private FileHandlerLinux fileHandler;
+
+    private RamLinux (){
+        
+    }
+
+    public static synchronized RamLinux instance(){
+        if(ramLinuxInstance == null){
+            ramLinuxInstance = new RamLinux();
+        }
+        return ramLinuxInstance;
+    }
     @Override
     public String capacity() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'capacity'");
+        return fileHandler.getTotalRAM();
     }
 
     @Override
     public String usage() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'usage'");
+        return fileHandler.getRAMInUse();
     }
     
 }
